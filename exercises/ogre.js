a5e2cf6a91ce9d245ee59b37e1d6337d83f3a4bc
Evette Telyas <etelyas@gmail.com>
2019-11-12 16:02:31 -0700

    ogre

---
+++ exercises/ogre.js
@@ -0,0 +1,27 @@
+class Ogre {
+	constructor(name, home = 'Swamp') {
+		this.name = name;
+		this.home = home;
+		this.swings = 0;
+	}
+
+	encounter(human) {
+		human.encounterCounter ++
+		if (human.noticesOgre() == true) {
+			this.swingAt(human)
+			if (this.swings % 2 == 0 && this.swings != 0 ) {
+				human.knockedOut = true;
+			}
+		}
+	}
+
+	swingAt(human) {
+		this.swings ++
+	}
+
+	apologize(human) {
+		human.knockedOut = false;
+	}
+}
+
+module.exports = Ogre;
