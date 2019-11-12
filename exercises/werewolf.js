b410eca209570c118d7382fad181692824eb6d83
Evette Telyas <etelyas@gmail.com>
2019-11-12 13:12:51 -0700

    warewolf

---
+++ exercises/werewolf.js
@@ -0,0 +1,29 @@
+class Werewolf {
+	constructor(name, location) {
+		this.name = name;
+		this.location = location;
+		this.human = true;
+		this.wolf = false;
+		this.hungry = false;
+	}
+
+	change() {
+		this.human = !this.human;
+		this.wolf = !this.wolf;
+		this.hungry = !this.hungry;
+	}
+
+	eat(victim) {
+		if (this.hungry == false) {
+			var str = 'I cannot eat because I am not hungry.'
+			return str
+		} else {
+			victim.alive = false;
+			this.change();
+			return 'YUM!'
+		}
+
+	}
+}
+
+module.exports = Werewolf;
